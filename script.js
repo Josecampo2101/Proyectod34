47e92e7277dba3d03c75f833f6696e5f7e1dbcca
Jose Campo <Josecampo2101@gmail.com>
2024-06-21 11:25:02 -0500

    Mejora Script

--- script.js
+++ script.js
@@ -26,13 +26,13 @@ const toggleButton = document.getElementById('toggleButton');
 const sidebar = document.querySelector('.sidebar');
 const sidebarItems = document.querySelectorAll('.sidebar nav ul li');
 
-let sidebarCollapsed = true; 
+let sidebarCollapsed = false; // Variable para mantener el estado de la barra lateral
 
 toggleButton.addEventListener('click', function() {
     sidebar.classList.toggle('collapsed');
     sidebarCollapsed = !sidebarCollapsed;
 
-
+    // Función para manejar la visibilidad de los submenús
     function handleSubMenuVisibility(item, submenu) {
         const link = item.querySelector('a');
         link.addEventListener('mouseenter', function() {
@@ -48,7 +48,7 @@ toggleButton.addEventListener('click', function() {
         });
     }
 
-
+    // Mostrar u ocultar submenús y texto de los menús principales
     sidebarItems.forEach(item => {
         const submenu = item.querySelector('ul.submenu');
         if (submenu) {
@@ -72,34 +72,17 @@ toggleButton.addEventListener('click', function() {
     });
 });
 
-
-const mediaQuery = window.matchMedia('(max-width: 768px)');
-
-function handleMobileLayout(mediaQuery) {
-    if (mediaQuery.matches) {
-        sidebar.classList.add('collapsed');
-        sidebarItems.forEach(item => {
-            const submenu = item.querySelector('ul.submenu');
-            if (submenu) {
-                submenu.style.display = 'none';
-            }
-
-            const link = item.querySelector('a');
-            if (link.querySelector('span')) {
-                link.querySelector('span').style.display = 'none';
-            }
-        });
-    } else {
-        sidebar.classList.remove('collapsed');
-        sidebarItems.forEach(item => {
-            const link = item.querySelector('a');
-            if (link.querySelector('span')) {
-                link.querySelector('span').style.display = 'inline';
-            }
-        });
+// Inicialmente, colapsar la barra lateral
+sidebar.classList.add('collapsed');
+sidebarCollapsed = true;
+sidebarItems.forEach(item => {
+    const submenu = item.querySelector('ul.submenu');
+    if (submenu) {
+        submenu.style.display = 'none';
     }
-}
 
-handleMobileLayout(mediaQuery); // Llamada inicial
-
-mediaQuery.addListener(handleMobileLayout); // Escuchar cambios en el media query
+    const link = item.querySelector('a');
+    if (link.querySelector('span')) {
+        link.querySelector('span').style.display = 'none';
+    }
+});
